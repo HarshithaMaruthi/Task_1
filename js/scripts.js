5fcf6204f9d4b1256943073268c2042d8d5616e8
HarshithaMaruthi <88500915+HarshithaMaruthi@users.noreply.github.com>
2021-08-11 16:21:53 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Fashion World - Landing Page v6.0.3 (https://fashionworld.com/theme/landing-page)
-* Copyright 2013-2021 Start Bootstrap
+* Copyright 2013-2021 Fashion World
 * Licensed under MIT (https://github.com/fashionworld/fashionworld-landing-page/blob/master/LICENSE)
 */
 // This file is intentionally blank
